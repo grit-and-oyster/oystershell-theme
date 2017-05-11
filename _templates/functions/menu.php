8d565c80f9dc32e753eb4bfd02cb9c914ac0e5aa
Andy Strange <code@grit-oyster.co.uk>
2017-05-11 13:41:56 +0100

    Added templates folder & some menu functions

---
+++ _templates/functions/menu.php
@@ -0,0 +1,72 @@
+<?php
+
+// The Top Menu
+function oystershell_top_nav() {
+	 wp_nav_menu(array(
+        'container' => false,                           // Remove nav container
+        'menu_class' => 'vertical medium-horizontal menu',       // Adding custom nav class
+        'items_wrap' => '<ul id="%1$s" class="%2$s" data-responsive-menu="accordion medium-dropdown">%3$s</ul>',
+        'theme_location' => 'primary',        			// Where it's located in the theme
+        'depth' => 5,                                   // Limit the depth of the nav
+        'fallback_cb' => false,                         // Fallback function (see below)
+        'walker' => new Topbar_Menu_Walker()
+    ));
+}
+
+// Big thanks to Brett Mason (https://github.com/brettsmason) for the awesome walker
+class Topbar_Menu_Walker extends Walker_Nav_Menu {
+    function start_lvl(&$output, $depth = 0, $args = Array() ) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "\n$indent<ul class=\"menu\">\n";
+    }
+}
+
+// The Off Canvas Menu
+function oystershell_off_canvas_nav() {
+	 wp_nav_menu(array(
+        'container' => false,                           // Remove nav container
+        'menu_class' => 'vertical menu',       // Adding custom nav class
+        'items_wrap' => '<ul id="%1$s" class="%2$s" data-accordion-menu>%3$s</ul>',
+        'theme_location' => 'primary',        			// Where it's located in the theme
+        'depth' => 5,                                   // Limit the depth of the nav
+        'fallback_cb' => false,                         // Fallback function (see below)
+        'walker' => new Off_Canvas_Menu_Walker()
+    ));
+}
+
+class Off_Canvas_Menu_Walker extends Walker_Nav_Menu {
+    function start_lvl(&$output, $depth = 0, $args = Array() ) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "\n$indent<ul class=\"vertical menu\">\n";
+    }
+}
+
+// The Footer Menu
+function oystershell_footer_links() {
+    wp_nav_menu(array(
+    	'container' => 'false',                         // Remove nav container
+    	'menu' => __( 'Footer Links', 'jointswp' ),   	// Nav name
+    	'menu_class' => 'menu',      					// Adding custom nav class
+    	'theme_location' => 'postscript',             // Where it's located in the theme
+        'depth' => 0,                                   // Limit the depth of the nav
+    	'fallback_cb' => ''  							// Fallback function
+	));
+} /* End Footer Menu */
+
+// Header Fallback Menu
+function oystershell_main_nav_fallback() {
+	wp_page_menu( array(
+		'show_home' => true,
+    	'menu_class' => '',      						// Adding custom nav class
+		'include'     => '',
+		'exclude'     => '',
+		'echo'        => true,
+        'link_before' => '',                           // Before each link
+        'link_after' => ''                             // After each link
+	) );
+}
+
+// Footer Fallback Menu
+function oystershell_footer_links_fallback() {
+	/* You can put a default here if you like */
+}
